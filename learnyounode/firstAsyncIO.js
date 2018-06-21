ae2c122e8276fed1fce52606422872a46fddddfa
frnk <777443+laurelton@users.noreply.github.com>
2018-06-21 17:54:33 -0400

    First Async

--- learnyounode/firstAsyncIO.js
+++ learnyounode/firstAsyncIO.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let fs = require('fs');
+fs.readFile(process.argv[2], { encoding: 'utf8' }, function(err, data) {
+  if (err) {
+    throw new Error(err);
+  }
+
+  let newLines = data.toString().match(/\n/g);
+  console.log(newLines.length);
+});
