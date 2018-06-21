bbd8921a7c51fabeb038cbefa23671b5688cafd2
frnk <777443+laurelton@users.noreply.github.com>
2018-06-21 14:42:45 -0400

    First 3 exercises

--- learnyounode/babySteps.js
+++ learnyounode/babySteps.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// let numbers = process.argv.slice(2);
+
+console.log(
+  process.argv
+    .slice(2)
+    .map(num => Number(num))
+    .reduce((total, num) => (total += num))
+);
--- learnyounode/firstIO.js
+++ learnyounode/firstIO.js
@@ -1,1 +1,8 @@
 'use strict';
+
+let fs = require('fs');
+let buffer = fs.readFileSync(process.argv[2]);
+let contents = buffer.toString();
+let newLines = contents.match(/\n/g);
+
+console.log(newLines.length);
