f818545b6848868bd34d320ae231d86d3b9796d2
frnk <777443+laurelton@users.noreply.github.com>
2018-06-22 16:56:27 -0400

    Filtered ls

--- learnyounode/filteredLS.js
+++ learnyounode/filteredLS.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let fs = require('fs');
+let directory = process.argv[2];
+let extension = '.' + process.argv[3];
+
+fs.readdir(directory, (err, list) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  list
+    .filter(file => file.endsWith(extension))
+    .forEach(file => console.log(file));
+});
