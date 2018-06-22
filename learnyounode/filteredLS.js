277256f433c63e0fd1341ef325449abc80ca5a46
Frank Rose <franklin.rose@gmail.com>
2018-06-21 21:40:05 -0400

    Filtered

--- learnyounode/filteredLS.js
+++ learnyounode/filteredLS.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let dir = process.argv[2];
+let extension = '.' + process.argv[3];
+
+let fs = require('fs');
+fs.readdir(dir, (err, list) => {
+    if (err) {
+        throw new Error(err);
+    }
+    // console.log('Before:', list);
+    list = list.filter(file => file.endsWith(extension));
+    // console.log('After:', list);
+    list.forEach(file => console.log(file));
+});
