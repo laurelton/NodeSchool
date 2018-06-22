91881812895357f8c28a67eebd5cfe9ceadab3ae
frnk <777443+laurelton@users.noreply.github.com>
2018-06-22 17:26:12 -0400

    Make it Modular

--- learnyounode/modular.js
+++ learnyounode/modular.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let fs = require('fs');
+let mod = require('./module.js');
+let path = process.argv[2];
+let ext = process.argv[3];
+
+mod(path, ext, (err, list) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  list.forEach(file => console.log(file));
+});
