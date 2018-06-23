38c29a8c088b1c47de9577b74880726ebf34cf73
Frank Rose <franklin.rose@gmail.com>
2018-06-23 17:32:50 -0400

    HTTP client

--- learnyounode/HTTPClient.js
+++ learnyounode/HTTPClient.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let http = require('http');
+let url = process.argv[2];
+
+http.get(url, (response) => {
+  response.setEncoding('utf8');
+  response.on('error', (err) => {
+    throw new Error(err);
+  });
+  response.on('data', (data) => {
+    data.split('\n').forEach(line => console.log(line));
+  });
+}).on('error', console.error);
